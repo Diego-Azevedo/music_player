555688afbadc958d64134919e1c3e7f9fab330b0
Diego-Azevedo <diegobeckerdeazevedo@gmail.com>
2022-10-23 21:20:18 -0300

    add - pausar, passar, voltar musica (player)

--- controlador/controlador_player.py
+++ controlador/controlador_player.py
@@ -3,30 +3,69 @@ from view.tela_player import TelaPlayer
 class ControladorPlayer:
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
+        self.__musicas_tocadas = []
         self.__tela_player = TelaPlayer()
 
-
-    def tocar_musica_aleatoria(self):
-        musica_aleatoria = self.__controlador_sistema.controlador_cadastro.retorna_musica_aleatoria()
-        self.__tela_player.mostra_musica(musica_aleatoria)
-
+    def abre_tela(self):
+        while True:
+            lista_opcoes = {1: self.escolher_musica, 2: self.tocar_musica_aleatoria, 
+                            0: self.retornar}
+            lista_opcoes[self.__tela_player.tela_opcoes()]()
 
     def escolher_musica(self):
         lista_musicas = self.__controlador_sistema.controlador_cadastro.retorna_musicas()
         self.__tela_player.print_escolher_musica()
         for index, item in enumerate(lista_musicas):
-            self.__tela_player.escolhe_musica(index, item)
+            self.__tela_player.dados_musica(index, item)
         opcao_escolhida = self.__tela_player.escolhe_opcao()       
         self.__tela_player.quebra_linha()
-        self.__tela_player.mostra_musica(lista_musicas[opcao_escolhida])    
+        self.__tela_player.print_tocando()
+        self.__tela_player.mostra_musica(lista_musicas[opcao_escolhida])
+        self.__musicas_tocadas.append(lista_musicas[opcao_escolhida])
+        self.abre_tela_player()            
 
+    def tocar_musica_aleatoria(self):
+        musica_aleatoria = self.__controlador_sistema.controlador_cadastro.retorna_musica_aleatoria()
+        self.__tela_player.print_tocando()
+        self.__tela_player.mostra_musica(musica_aleatoria)
+        self.__musicas_tocadas.append(musica_aleatoria)
+        self.abre_tela_player()
+    
     def retornar(self):
         self.__controlador_sistema.abre_tela()
 
-    def abre_tela(self):
-        lista_opcoes = {1: self.escolher_musica, 2: self.tocar_musica_aleatoria, 
-                        0: self.retornar}
+    def abre_tela_player(self):
+        while True:
+            lista_opcoes2 = {1: self.pausar_musica, 2: self.passar_musica,
+                             3: self.voltar_musica, 0:self.abre_tela}
+            lista_opcoes2[self.__tela_player.player_opcoes()]()                         
 
-        continua = True
-        while continua:
-            lista_opcoes[self.__tela_player.tela_opcoes()]()
+    def passar_musica(self):
+        lista_musicas = self.__controlador_sistema.controlador_cadastro.retorna_musicas()
+        musica_atual = self.__musicas_tocadas[-1]
+        for index, indice in enumerate(lista_musicas):
+            if indice['nome'] == musica_atual['nome']:
+                n = index
+        if n == (len(lista_musicas) - 1):
+            n = 0
+        self.__tela_player.print_tocando()        
+        self.__tela_player.mostra_musica(lista_musicas[n+1])
+        self.__musicas_tocadas.append(lista_musicas[n+1])        
+
+    def pausar_musica(self):
+        musica_atual = self.__musicas_tocadas[-1]
+        self.__tela_player.pausar_musica()
+        self.__tela_player.print_tocando()
+        self.__tela_player.mostra_musica(musica_atual)
+
+    def voltar_musica(self):
+        lista_musicas = self.__controlador_sistema.controlador_cadastro.retorna_musicas()
+        musica_atual = self.__musicas_tocadas[-1]
+        for index, indice in enumerate(lista_musicas):
+            if indice['nome'] == musica_atual['nome']:
+                n = index
+        if n == 0:
+            n = len(lista_musicas)
+        self.__tela_player.print_tocando()        
+        self.__tela_player.mostra_musica(lista_musicas[n-1])
+        self.__musicas_tocadas.append(lista_musicas[n-1])                    
--- view/tela_player.py
+++ view/tela_player.py
@@ -10,17 +10,34 @@ class TelaPlayer(TelaAbstrata):
         print()
         return opcao
 
+    def player_opcoes(self):
+        print("____PLAYER OPCÕES____")
+        print("1 - Pausar")
+        print("2 - Passar Música")
+        print("3 - Voltar Música")
+        print("0 - Sair")
+        opcao2 = int(input("Escolha a Opção: "))
+        print()
+        return opcao2
+
+    def print_tocando(self):
+        print("--------TOCANDO--------")            
+
     def mostra_musica(self, nome):
-        print("--------TOCANDO--------")
         print("MÚSICA:", nome["nome"], "-", nome["artista"])
         print()
 
-    def escolhe_musica(self, index, dados_musica):
+    def dados_musica(self, index, dados_musica):
         print(index, "-", dados_musica["nome"], "-", dados_musica["artista"])
 
     def escolhe_opcao(self):
         opcao = int(input("Escolha a Opção: "))
         return opcao
+
+    def pausar_musica(self):
+        print("--------MÚSICA PAUSADA--------")
+        input("Clique para continuar: ")
+        print()    
 
     def print_escolher_musica(self):
         print("--------ESCOLHER MÚSICA--------")        
