ca091d9a20d0cd7328ead30113d379fce491bdb6
joaopedrotheodoro <joaopedrth@gmail.com>
2022-12-10 21:29:59 -0300

    Interface na tela registro

--- view/tela_registro.py
+++ view/tela_registro.py
@@ -1,45 +1,100 @@
 from view.tela_abstrata import TelaAbstrata
+import PySimpleGUI as sg
 
 class TelaRegistro(TelaAbstrata):
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
 
     def tela_opcoes(self):
-        print("____REGISTRO____")
-        print("1 - Criar Playlist")
-        print("2 - Excluir Playlist")
-        print("3 - Histórico do Player")
-        print("4 - Limpar Histórico")
-        print("0 - Voltar")
-        opcao = self.ler_opcao("Escolha a opcao: ", [0, 1, 2, 3, 4])
-        print()
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['0'] or button in (None,'Cancelar'):
+            opcao = 0
+        self.close()
         return opcao
 
-    def nome_playlist(self):
-        nome = input("Nome da Playlist: ")
+
+    def init_opcoes(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+            [sg.Text('Player de Música', font=("Helvica",25))],
+            [sg.Text('Escolha sua opção', font=("Helvica",15))],
+            [sg.Radio('Criar Playlist',"RD1", key='1')],
+            [sg.Radio('Excluir Playlist',"RD1", key='2')],
+            [sg.Radio('Histórico do Player',"RD1", key='3')],
+            [sg.Radio('Limpar Histórico',"RD1", key='4')],
+            [sg.Radio('Sair',"RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Player de Música').Layout(layout)
+
+    def seleciona_musica(self):
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+        [sg.Text('-------- SELECIONAR PLAYLIST ----------', font=("Helvica", 25))],
+        [sg.Text('Digite o nome da playlist:', font=("Helvica", 15))],
+        [sg.Text('NOME:', size=(15, 1)), sg.InputText('', key='nome')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Seleciona musica').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        self.close()
         return nome
 
-    def escolher_musica(self):
-        posicao = int(input("Escolha uma música: "))
-        return posicao
+    def seleciona_musica(self):
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+        [sg.Text('-------- SELECIONAR MÚSICA ----------', font=("Helvica", 25))],
+        [sg.Text('Digite o nome da mùsica:', font=("Helvica", 15))],
+        [sg.Text('NOME:', size=(15, 1)), sg.InputText('', key='nome')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Seleciona musica').Layout(layout)
 
-    def escolher_musica(self):
-        posicao = int(input("Escolha uma playlist: "))
+        button, values = self.open()
+        posicao = values['nome']
+        self.close()
         return posicao              
 
     def mostra_musica(self, index, dados_musica):
-        print(index, "-", "MÚSICA:", dados_musica["nome"], "-", "ARTISTA:", dados_musica["artista"])
+        [sg.Text(index, "-", "MÚSICA:", dados_musica["nome"], "-", "ARTISTA:", dados_musica["artista"])]
 
     def mostra_playlist(self, index, nome):
-        print(index, "-", nome)    
+        [sg.Text(index, '-'), nome]    
 
     def mostra_musica_adicionada(self, msg, musica, mensagem, playlist):
-        print(msg, musica, mensagem, playlist)
+        [sg.Text(msg, musica, mensagem, playlist)]
 
     def mostra_registro(self, ouvidas, curtidas):
-        print("Você escutou um total de", ouvidas, "músicas, e favoritou", curtidas, "músicas.")
-        print()        
+        [sg.Text('Você escutou um total de', ouvidas, 'músicas, e favoritou', curtidas, 'músicas.', font=("Helvica",25))],
 
     def continuar(self):
+        [sg.Text('Escolha sua opção', font=("Helvica",15))],
+        [sg.Radio('Continuar',"RD1", key='1')],
+        [sg.Radio('Salvar e Sair',"RD1", key='2')],
         print("1 - Continuar")
         print("0 - Salvar e Sair")
         retorno = input("Escolha a Opção: ")
         return retorno
+
+    def mostra_mensagem(self, msg):
+        sg.popup("", msg)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
